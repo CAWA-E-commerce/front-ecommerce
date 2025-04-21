75abc4e331f025c816c2cfd91fee086316080cc3
Ghalem Salim <salimghalem40@gmail.com>
2025-04-22 00:19:09 +0100

    nice landing page done still panier page to do

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Container, Card, CardContent, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
+import Threads from '../Backgrounds/Threads/Threads';
 
 const Home = () => {
   const [error, setError] = useState(null);
@@ -36,13 +37,49 @@ const Home = () => {
   }, []);
 
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>
-        Products
-      </Typography>
+    <div
+      style={{
+        overflow: 'hidden',
+      }}
+    >
+      <div
+        style={{
+          width: '100%',
+          height: '100vh',
+          position: 'relative',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+          paddingBlock: '10vh',
+        }}
+      >
+        <Typography
+          variant="h1"
+          style={{
+            alignSelf: 'center',
+            color: 'white',
+            fontFamily: 'title',
+          }}
+        >
+          ByteVault
+        </Typography>
+        <Typography
+          variant="h4"
+          style={{
+            alignSelf: 'center',
+            color: 'white',
+            fontFamily: 'title',
+          }}
+        >
+          All you need right here .
+        </Typography>
+        <Threads amplitude={1} distance={0} enableMouseInteraction={true} />
+      </div>
+
       {error && <Typography color="error">{error}</Typography>}
       <div ref={resultRef} />
-    </Container>
+    </div>
   );
 };
 
